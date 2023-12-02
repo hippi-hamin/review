2f884b2729f8df8693ab358b92d55bae9c3913c2
hippi-hamin <9908haha@naver.com>
2023-12-02 20:41:46 +0900

    산술연산자

--- src/firstWeek/Variables.java
+++ src/firstWeek/Variables.java
@@ -3,8 +3,12 @@ package firstWeek;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num1 = 10; // 10은 정수
+		int num2; // num2는 값은 없지만 정수형 변수임을 선언
+		
+		double num3 = 1.101; // 실수(소수)의 변수 선언
+		boolean num4 = true; // true or false 내용의 변수 선언
+		String num5 = "문장";   // 문장및 부호 변수 선언
 	}
 
 }
