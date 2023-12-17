28df25901659cae0a991475ce0353b777671856e
hippi-hamin <9908haha@naver.com>
2023-12-17 20:06:34 +0900

    Book review start

---
+++ src/classesReview/ex1/BookRepository.java
@@ -0,0 +1,44 @@
+package classesReview.ex1;
+
+public class BookRepository {
+    // 도서 정보를 관리하는 bookDTOList
+
+    /**
+     * 도서등록 메서드
+     * name: save
+     * parameter: BookDTO
+     * return: boolean
+     * 실행내용
+     *      Service로 부터 전달 받은 DTO 객체를 리스트에 저장하고 결과를 리턴
+     */
+
+    /**
+     * 도서목록 메서드
+     * name: findAll
+     * parameter: x
+     * return: List<BookDTO>
+     * 실행내용
+     *      Service로 부터 호출되면 리스트를 리턴
+     */
+
+    /**
+     * 도서조회 메서드
+     * name: findById
+     * parameter: Long
+     * return: BookDTO
+     * 실행내용
+     *      Service로 부터 id를 전달받고 일치하는 결과를 찾아서 DTO를 리턴
+     *      없으면 null 리턴
+     */
+
+    /**
+     * 도서조회 메서드
+     * name: findByTitle
+     * parameter: String
+     * return: BookDTO
+     * 실행내용
+     *      Service로 부터 도서제목을 전달받고 일치하는 결과를 찾아서 DTO를 리턴
+     *      없으면 null 리턴
+     */
+
+}
---
+++ src/classesReview/ex1/BookService.java
@@ -0,0 +1,45 @@
+package classesReview.ex1;
+
+public class BookService {
+    /**
+     * 도서등록 메서드
+     * name: save
+     * parameter: x
+     * return: x
+     * 실행내용
+     *      스캐너로 도서명, 저자, 가격, 출판사정보를 입력받고
+     *      BookDTO 객체에 담아서 Repository로 전달하여 저장이 되도록 함
+     *      등록 여부를 출력한다.(등록성공 or 등록실패)
+     */
+
+    /**
+     * 도서목록 메서드
+     * name: findAll
+     * parameter: x
+     * return: x
+     * 실행내용
+     *      Repository로 부터 목록을 리턴 받아서 목록에 있는 모든 정보를 출력
+     */
+
+    /**
+     * 도서조회 메서드
+     * name: findById
+     * parameter: x
+     * return: x
+     * 실행내용
+     *      id를 입력받고 Repository로 부터 id에 해당 하는 도서 정보를 출력
+     *      없으면 없다고 출력
+     */
+
+    /**
+     * 도서조회 메서드
+     * name: findByTitle
+     * parameter: x
+     * return: x
+     * 실행내용
+     *      도서제목을 입력받고 Repository로 부터 해당 하는 도서 정보를 출력(제목이 정확히 일치하는 것만)
+     *      없으면 없다고 출력
+     */
+}
+
+}
